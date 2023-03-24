70dcedc41aeaf8723e25a8fe0fd48ea20eca1d6e
Dan Meleras <meleras.daniel@icloud.com>
2023-03-24 15:23:45 -0700

    fixed numeric data type issue

--- models/money_supply/money_supply_models.py
+++ models/money_supply/money_supply_models.py
@@ -1,4 +1,5 @@
 from database import db, BaseModel
+from models.custom_types.custom_types import CustomNumeric
 
 class MoneySupplyTable(BaseModel):
     __tablename__ = 'money_supply'
@@ -6,6 +7,6 @@ class MoneySupplyTable(BaseModel):
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date)
-    value = db.Column(db.Numeric(10, 4))
+    value = db.Column(CustomNumeric(10, 4))
     type = db.Column(db.String(2))
     country = db.Column(db.String(3))
