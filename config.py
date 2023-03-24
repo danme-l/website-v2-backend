5d86dcff7af7b93ab360c4e877fe1f492b6f7b90
Dan Meleras <meleras.daniel@icloud.com>
2023-03-23 20:30:59 -0700

    more config fixes

--- config.py
+++ config.py
@@ -6,10 +6,3 @@ class Config:
 
     # SQLAlchemy doesn't accept 'postgres://' anymore, but this is render's default
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
-
-class TestConfig:
-    # old config string
-    env = dotenv_values(".env")
-
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{env['PGUSER']}:{env['PGPWD']}@{env['PGHOST']}/{env['PGDB']}"
-    # postgresql://{env['PGUSER']}:{env['PGPWD']}@{env['PGHOST']}/{env['PGDB']}
