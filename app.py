8e92ee585661ce05919cc2bc0b24ec6b43bb9186
Dan Meleras <meleras.daniel@icloud.com>
2023-03-23 20:08:33 -0700

    config fixes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from config import Config
+from config import Config, TestConfig
 from flask_cors import CORS, cross_origin
 from database import db
 
@@ -14,10 +14,12 @@ def create_app(config_class=Config):
     # import controllers
     from controllers.weo.weo_controller import weo_bp
     from controllers.econ.econ_controller import econ_bp
+    from controllers.money_supply.money_supply_controller import money_supply_bp
 
     # register controllers
     app.register_blueprint(weo_bp)
     app.register_blueprint(econ_bp, url_prefix='/econ')
+    app.register_blueprint(money_supply_bp)
 
     # test route
     @app.route('/')
@@ -41,7 +43,6 @@ def add_cors_headers(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
-
-
 if __name__ == '__main__':
+    app = create_app(TestConfig)
     app.run()
--- config.py
+++ config.py
@@ -4,11 +4,12 @@ from dotenv import dotenv_values
 class Config:
     """Production Config object"""
 
+    # SQLAlchemy doesn't accept 'postgres://' anymore, but this is render's default
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
+
+class TestConfig:
     # old config string
     env = dotenv_values(".env")
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{env['PGUSER']}:{env['PGPWD']}@{env['PGHOST']}/{env['PGDB']}"
     # postgresql://{env['PGUSER']}:{env['PGPWD']}@{env['PGHOST']}/{env['PGDB']}
-
-    # SQLAlchemy doesn't accept 'postgres://' anymore, but this is render's default
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
