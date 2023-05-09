a1e9dbfc47c8c8057e949c19019433351728a64a
Dan Meleras <meleras.daniel@icloud.com>
2023-05-09 14:47:00 -0400

    oil controller CORS fix

--- controllers/oil/oil_controller.py
+++ controllers/oil/oil_controller.py
@@ -6,7 +6,7 @@ from flask_cors import cross_origin
 oil_production_bp = Blueprint('oil_production', __name__)
 
 @oil_production_bp.route('/oil_production', methods=['GET'])
-@cross_origin()
+#@cross_origin()
 def oil_production():
     oil_production = db.session.query(OilProductionTable).all()
     return jsonify([rec.serialize() for rec in oil_production])
